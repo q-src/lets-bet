a29f8aa4182924b283e945322f79f4ff679a3bb0
Adrian Pauli <pauli.adrian96@gmail.com>
2019-12-05 01:33:55 +0100

    Add minimal auth configuration with noop user

--- backend/grails-app/conf/application.groovy
+++ backend/grails-app/conf/application.groovy
@@ -1,13 +1,9 @@
-grails.plugin.springsecurity.filterChain.chainMap = [
-        //Stateless chain
-        [
-                pattern: '/**',
-                filters: 'JOINED_FILTERS,-anonymousAuthenticationFilter,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter'
-        ],
-
-        //Traditional, stateful chain
-        [
-                pattern: '/stateful/**',
-                filters: 'JOINED_FILTERS,-restTokenValidationFilter,-restExceptionTranslationFilter'
-        ]
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'letsbet.User'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+        [pattern: '/**', access: ['IS_AUTHENTICATED_FULLY']],
 ]
+
+grails.plugin.springsecurity.filterChain.chainMap = [
+        [pattern: '/**', filters: 'JOINED_FILTERS,-anonymousAuthenticationFilter,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter']
+]
+
--- backend/grails-app/init/letsbet/BootStrap.groovy
+++ backend/grails-app/init/letsbet/BootStrap.groovy
@@ -3,6 +3,7 @@ package letsbet
 class BootStrap {
 
     def init = { servletContext ->
+        new User(username: "user1", password: "{noop}password").save()
     }
     def destroy = {
     }
