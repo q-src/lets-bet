466f427f7c62d7368b4ce275349976f346564460
Adrian Pauli <pauli.adrian96@gmail.com>
2019-12-07 14:25:59 +0100

    Add bet module

---
+++ frontend/src/app/bet/bet.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BetRoutingModule } from './bet-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BetRoutingModule
+  ]
+})
+export class BetModule { }
