fa0b93317d3cfc8018950633db90979f2be424db
Adrian Pauli <pauli.adrian96@gmail.com>
2019-12-07 21:40:30 +0100

    Reduce properties on bet entity to those required for creation

--- backend/grails-app/domain/letsbet/Bet.groovy
+++ backend/grails-app/domain/letsbet/Bet.groovy
@@ -6,25 +6,15 @@ import grails.rest.*
 @Resource(readOnly = false, formats = ['json', 'xml'])
 class Bet {
 
-    User owner
-    User opponent
     User commissioner
-
-    String ownerStatement
-    String opponentStatement
-    String commissionerStatement
-
-    String ownerBet
-    String opponentBet
+    String title
+    String description
     String rules
-
     Status status
 
     enum Status {
         CREATED,
-        NEGOTIATED,
-        WON,
-        LOST,
-        DRAW
+        STARTED,
+        COMPLETED,
     }
 }
