a09117b42736ea18365d2216a3b6d3389c1a353c
Adrian Pauli <pauli.adrian96@gmail.com>
2019-12-07 14:36:45 +0100

    Configure routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'bet',
+    loadChildren: () => import('./bet/bet.module').then(mod => mod.BetModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/bet/bet-routing.module.ts
+++ frontend/src/app/bet/bet-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BetCreateComponent} from './bet-create/bet-create.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'create',
+    component: BetCreateComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
