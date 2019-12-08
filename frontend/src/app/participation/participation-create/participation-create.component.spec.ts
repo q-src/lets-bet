afc39638544bbc52f3969617848b97892e6412c8
Adrian Pauli <pauli.adrian96@gmail.com>
2019-12-08 19:23:21 +0100

    Fix test

--- frontend/src/app/participation/participation-create/participation-create.component.spec.ts
+++ frontend/src/app/participation/participation-create/participation-create.component.spec.ts
@@ -4,17 +4,25 @@ import { ParticipationCreateComponent } from './participation-create.component';
 import { Bet } from '../../bet-api/bet';
 import { ActivatedRoute } from '@angular/router';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ParticipationApiService } from '../../participation-api/participation-api.service';
+import createSpyObj = jasmine.createSpyObj;
 
 describe('ParticipationCreateComponent', () => {
   let component: ParticipationCreateComponent;
   let fixture: ComponentFixture<ParticipationCreateComponent>;
+  let api: ParticipationApiService;
   const bet: Bet = {} as Bet;
 
   beforeEach(async(() => {
+    api = createSpyObj('BetApiService', ['find']);
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [ ParticipationCreateComponent ],
       providers: [
+        {
+          provide: ParticipationApiService,
+          useValue: api
+        },
         {
           provide: ActivatedRoute,
           useValue: {
