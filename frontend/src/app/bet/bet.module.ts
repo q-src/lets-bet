802823a382256684267920609040e4a99e21314c
Adrian Pauli <pauli.adrian96@gmail.com>
2019-12-09 18:03:21 +0100

    Add some links

--- frontend/src/app/bet/bet.module.ts
+++ frontend/src/app/bet/bet.module.ts
@@ -5,7 +5,7 @@ import { BetRoutingModule } from './bet-routing.module';
 import { BetCreateComponent } from './bet-create/bet-create.component';
 import { BetFormComponent } from './bet-form/bet-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatListModule, MatStepperModule } from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatStepperModule } from '@angular/material';
 import { BetApiModule } from '../bet-api/bet-api.module';
 import { BetEditComponent } from './bet-edit/bet-edit.component';
 import { BetListComponent } from './bet-list/bet-list.component';
@@ -21,6 +21,7 @@ import { QRCodeModule } from 'angularx-qrcode';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
+    MatIconModule,
     MatListModule,
     MatStepperModule,
     QRCodeModule,
